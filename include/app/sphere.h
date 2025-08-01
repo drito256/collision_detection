510e11b4465582f88664bb1de6378b9e7524ec94
fring <dsaric53@gmail.com>
2025-08-01 04:51:00 +0200

    Update sphere class

--- include/app/sphere.h
+++ include/app/sphere.h
@@ -2,21 +2,30 @@
 #define SPHERE_H
 
 #include "../glm/glm.hpp"
+#include "../glad/glad.h"
 
 class Sphere{
 public:
-    Sphere(glm::vec3 coordinate, glm::vec3 velocity, double radius);
+    Sphere(glm::vec3 coordinate, glm::vec3 velocity, float radius);
     glm::vec3 get_coordinate();
     glm::vec3 get_velocity();
-    double get_radius();
+    float get_radius();
 
     void update();
+    void render();
 
 private:
     glm::vec3 coordinate;
     glm::vec3 velocity;
-    double radius;
+    float radius;
+
+    unsigned int vao;
+    unsigned int vbo;
 };
 
+struct Vertex{
+    glm::vec3 coordinate;
+    float radius;
+};
 
 #endif
--- src/sphere.cpp
+++ src/sphere.cpp
@@ -1,12 +1,33 @@
 #include "../include/app/sphere.h"
 
 
-Sphere::Sphere(glm::vec3 coordinate, glm::vec3 velocity, double radius) : coordinate{coordinate}, 
+Sphere::Sphere(glm::vec3 coordinate, glm::vec3 velocity, float radius) : coordinate{coordinate}, 
                                                                           velocity{velocity},
                                                                           radius{radius}
-                                                                          {}
+                                                                          {
+    glGenVertexArrays(1, &this->vao);
+    glGenBuffers(1, &this->vbo);
 
-double Sphere::get_radius(){
+    glBindVertexArray(this->vao);
+    glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+    
+    Vertex vertex{coordinate, radius};
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec3), &vertex , GL_DYNAMIC_DRAW);
+   
+    glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, sizeof(glm::vec3), (void*)offsetof(Vertex, coordinate));
+    glEnableVertexAttribArray(0);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float), &vertex, GL_DYNAMIC_DRAW);
+   
+    glVertexAttribPointer(1,1,GL_FLOAT, GL_FALSE, sizeof(double), (void*)offsetof(Vertex, radius));
+    glEnableVertexAttribArray(1);
+    
+
+    
+    glBindVertexArray(0);
+}
+
+float Sphere::get_radius(){
     return this->radius;
 }
 
